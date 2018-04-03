23a2d9c04a832d856cb592f976a2cfaed4ce919c
BijanSadeghi <sadeghi.bijan@gmail.com>
2018-04-03 13:03:15 -0400

    Add SearchState Enumeration

---
+++ CaptureTheFlag/src/ca/mcgill/ecse211/enumeration/SearchState.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse211.enumeration;
+
+/**
+ * Enumeration for the state of the flag search.
+ * The search is either in progress, timed out,
+ * or the flag has been found.
+ * 
+ * @author bijansadeghi
+ *
+ */
+public enum SearchState {
+	IN_PROGRESS, TIMED_OUT, FLAG_FOUND
+}
